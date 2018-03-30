ca39f888ba79878a5e3147215ed0b51d804876ff
jim wu <jim_wu@trend.com.tw>
2018-03-30 14:35:46 +0800

    change socket connect

--- js/client.js
+++ js/client.js
@@ -1,7 +1,6 @@
 (function(){
 	//var socket = io.connect('http://avalon-test.herokuapp.com/');
-	var socket = io.connect('http://my-avalon.herokuapp.com/');
-	//var socket = io.connect('localhost:8080');
+	var socket = io.connect('localhost:80');
 	var gb = null 
 	var roomNumber = null ;
 	var role = null ;
